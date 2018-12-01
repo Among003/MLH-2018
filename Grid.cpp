a382eb79f99481380bb0420ae0d739674c2998ab
Adrian <among003@ucr.edu>
2018-12-01 12:22:34 -0800

    updated Grid cpp

--- Grid.cpp
+++ Grid.cpp
@@ -1,5 +1,20 @@
 #include "Grid.h"
 
 
+Grid::Grid(){
+	memset(m, 0, sizeof Grid);
+}
+
+Grid::~Grid(){
+}
+
+void Grid:: setShip(Ship*, int x, int y){
+
+}
+
+void Grid:: setHit(int x, int y){
+
+
+}
 
 

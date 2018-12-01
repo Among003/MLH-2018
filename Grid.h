970bf155c3d24b5aa895b8d89da168b9ef2a6d2e
danay001 <danay001@ucr.edu>
2018-12-01 12:05:05 -0800

    Init Classes

--- Grid.h
+++ Grid.h
@@ -1,7 +1,16 @@
 #ifndef __GRID_H__
 #define __GRID_H__
 
-
+class Grid{
+  public:
+	Grid();
+	~Grid();
+	void setShip(Ship*, int x, int y);
+	void setHit(int x, int y);
+  private:
+  	int GameBoard [10][10]; 
+	
+};
 
 
 

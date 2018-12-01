3dd162612d57b18898feac037af7e08c955417dc
danay001 <danay001@ucr.edu>
2018-12-01 13:47:59 -0800

    Add printBoard()

--- Grid.cpp
+++ Grid.cpp
@@ -2,7 +2,7 @@
 
 
 Grid::Grid(){
-	memset(m, 0, sizeof Grid);
+	memset(GameBoard, 0, sizeof(GameBoard));
 }
 
 Grid::~Grid(){
@@ -18,9 +18,24 @@ void Grid:: setHit(int x, int y){
 }
 
 void Grid::printBoard(){
+	system("clear");
+	std::cout << "--------------------------------------------------" << std::endl;
+	for(int i = 0; i < 10; ++i){
+		for(int j = 0; j < 10; ++j){
+			std::cout << '|';
+			if(GameBoard[i][j] == 0){
+				std::cout << " 0 |";
+			}
+			else if(GameBoard[i][j] == 1){
+				std::cout << " ~ |";
+			}
+			else if(GameBoard[i][j] == 2){
+				std::cout << " X |";
+			}
+		}
+		std::cout << std:: endl << "--------------------------------------------------" << std::endl;
 
-
-
-};
+	}
+}
 
 
